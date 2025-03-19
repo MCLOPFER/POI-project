e783fce923076445c68e208d1acec35ab4cb7035
Mari Cruz Lopez Fernandez <maricruzlop@gmail.com>
2025-03-19 15:15:35 +0000

    sessions-and-authentication

--- src/controllers/find-controller.js
+++ src/controllers/find-controller.js
@@ -2,9 +2,14 @@ import { db } from "../models/db.js";
 
 export const findController = {
   index: {
-    handler: function (request, h) {
+    handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
+      const points = await db.pointStore.getUserPoints(loggedInUser._id);
+
       const viewData = {
-        title: "Find a location",
+        title: "Place Mark Dashboard",
+        user: loggedInUser,
+        points: points,
       };
       return h.view("find-view", viewData);
     },
@@ -12,7 +17,9 @@ export const findController = {
 
   addPoint: {
     handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
       const newPoint = {
+        userid: loggedInUser._id,
         name: request.payload.name,
         description: request.payload.description
       };
