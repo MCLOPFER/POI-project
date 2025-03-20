a131ea689de8b2478109ce39ff7b357167af729d
Mari Cruz Lopez Fernandez <maricruzlop@gmail.com>
2025-03-20 12:03:12 +0000

    feature/joi-validation+json-data-store

--- src/controllers/find-controller.js
+++ src/controllers/find-controller.js
@@ -1,4 +1,5 @@
 import { db } from "../models/db.js";
+import { PointSpec } from "../models/joi-schemas.js";
 
 export const findController = {
   index: {
@@ -16,12 +17,18 @@ export const findController = {
   },
 
   addPoint: {
+    validate: {
+      payload: PointSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("dashboard-view", { title: "Add Point error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
       const newPoint = {
         userid: loggedInUser._id,
-        name: request.payload.name,
-        description: request.payload.description
+        name: request.payload.name
       };
       await db.pointStore.addPoint(newPoint);
       return h.redirect("/dashboard");
